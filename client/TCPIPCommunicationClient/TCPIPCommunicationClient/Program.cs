6478688063d32210e3f682ce24c200e07d888bbe
anarchysmo <73321844+anarchysmo@users.noreply.github.com>
2022-03-30 21:44:12 -0700

    Add files via upload

---
+++ client/TCPIPCommunicationClient/TCPIPCommunicationClient/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Sockets;
+using System.IO;
+using System.Text;
+
+namespace TCPIPCommunicationClient
+{
+    class Program   //client side
+    {
+        static void Main(string[] args)
+        {
+        connection:
+            try
+            {
+                //sending data
+                TcpClient client = new TcpClient("127.0.0.1", 8080);
+                string message = "Hi, I am client. U hear me?";
+
+                byte[] sendData = new byte[Encoding.ASCII.GetByteCount(message) + 1];
+                sendData = Encoding.ASCII.GetBytes(message);
+
+                NetworkStream stream = client.GetStream();
+                stream.Write(sendData, 0, sendData.Length);
+                Console.WriteLine("sending data to server...");
+
+                //receiving data from server
+
+                StreamReader reader = new StreamReader(stream);
+                Console.WriteLine(reader.ReadLine());
+
+                stream.Close();
+                client.Close();
+
+                Console.ReadKey();
+            }
+            catch
+            {
+                Console.WriteLine("failed to connect...");
+                goto connection;
+            }
+        }
+    }
+}
---
+++ server/TCPIPCommunication/TCPIPCommunication/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net.Sockets;
+using System.IO;
+using System.Text;
+
+namespace TCPIPCommunication
+{
+    class Program   //server side
+    {
+        static void Main(string[] args)
+        {
+            TcpListener listener = new TcpListener(System.Net.IPAddress.Any, 8080);
+            listener.Start();
+            while (true) 
+            {
+                Console.WriteLine("waiting for a connection");
+                TcpClient client = listener.AcceptTcpClient();
+                Console.WriteLine("client accepted");
+                NetworkStream stream = client.GetStream();
+                StreamReader reader = new StreamReader(client.GetStream());
+                StreamWriter writer = new StreamWriter(client.GetStream());
+                try
+                {
+                    byte[] buffer = new byte[1024];
+                    stream.Read(buffer, 0, buffer.Length);
+                    //real bytes counter
+                    int recv = 0;
+                    foreach (byte b in buffer) 
+                    {
+                        if (b != 0)
+                            recv++;
+                    }
+                    string request = Encoding.UTF8.GetString(buffer, 0, recv);
+                    Console.WriteLine("request received: " + request);
+                    writer.WriteLine("daaaam boy, you did it");
+                    writer.Flush(); //sending the message to client
+
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("something crashed...");
+                    writer.WriteLine(e.ToString());
+                }
+            }
+        }
+    }
+}
